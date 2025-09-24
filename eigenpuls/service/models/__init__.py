082eb3aac10e06aa496b812cf8c1ac0c45ec9661
Rouven Raudzus <raudzus@autiwire.org>
2025-09-24 23:08:46 +0200

    Add service registry and base service imports in models initialization

--- eigenpuls/service/models/__init__.py
+++ eigenpuls/service/models/__init__.py
@@ -1,1 +1,3 @@
+from eigenpuls.service.registry import service_registry
+from eigenpuls.service.base import KnownServiceType, Service, SystemPackageType, ServiceStatus, ServiceHealth
 from .rabbitmq import RabbitMQService
