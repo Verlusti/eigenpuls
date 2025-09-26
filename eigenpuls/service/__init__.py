9feeeaf0f6e0296b6cd601fd67e0cd65d30284fd
Rouven Raudzus <raudzus@autiwire.org>
2025-09-26 23:05:06 +0200

    Add Celery service implementations to KnownServiceType in service initialization

--- eigenpuls/service/__init__.py
+++ eigenpuls/service/__init__.py
@@ -3,6 +3,7 @@ from .base import KnownServiceType, Service, SystemPackageType, ServiceStatus, S
 from .models.rabbitmq import RabbitMQService
 from .models.postgres import PostgresService
 from .models.redis import RedisService
+from .models.celery import CeleryWorkerService, CeleryBeatService, CeleryFlowerService
 
 from typing import Dict, Type
 
@@ -11,6 +12,9 @@ Implementations: Dict[KnownServiceType, Type[Service]] = {
     KnownServiceType.RABBITMQ: RabbitMQService,
     KnownServiceType.POSTGRES: PostgresService,
     KnownServiceType.REDIS: RedisService,
+    KnownServiceType.CELERY_WORKER: CeleryWorkerService,
+    KnownServiceType.CELERY_BEAT: CeleryBeatService,
+    KnownServiceType.CELERY_FLOWER: CeleryFlowerService,
 }
 
 
