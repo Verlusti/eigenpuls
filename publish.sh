5cba6c9060d28d1b56b99437cab625667f980947
Rouven Raudzus <rouven.raudzus@gmail.com>
2025-10-04 21:44:41 +0200

    Remove publish and rebuild scripts to streamline the build process and eliminate unnecessary files.

--- publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export SETUPTOOLS_SCM_LOCAL_SCHEME=no-local-version
-python -m twine upload dist/*
--- rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export SETUPTOOLS_SCM_LOCAL_SCHEME=no-local-version
-rm -rf dist build *.egg-info
-python -m pip install -U build twine
-python -m build
